5f51ff43b2334dd88506507a28fab892f3f9daf8
Hal Botley <halbotley@users.noreply.github.com>
2026-02-03 07:59:36 -0800

    calmly 1.1.0: Add timed event support

--- Formula/calmly.rb
+++ Formula/calmly.rb
@@ -1,7 +1,8 @@
 class Calmly < Formula
   desc "A calm CLI for macOS Calendar. No dialogs, no prompts, just events."
   homepage "https://github.com/halbotley/calmly"
-  url "https://github.com/halbotley/calmly.git", tag: "v0.1.0"
+  url "https://github.com/halbotley/calmly/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "a53c2b22775bce9ae344993fa3d3d42718e0ea1c2669dd2769b95ba381022b76"
   license "MIT"
   head "https://github.com/halbotley/calmly.git", branch: "main"
 
@@ -13,6 +14,6 @@ class Calmly < Formula
   end
 
   test do
-    system "#{bin}/calmly", "list"
+    system "#{bin}/calmly", "version"
   end
 end
