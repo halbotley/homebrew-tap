1adb1f5e45fc47c94405867c850ab15680690036
Hal Botley <halbotley@users.noreply.github.com>
2026-02-03 13:32:57 -0800

    Add duely formula v1.0.0

--- Formula/duely.rb
+++ Formula/duely.rb
@@ -1,11 +1,11 @@
 class Duely < Formula
-  desc "Track recurring maintenance tasks"
+  desc "A calm CLI for tracking recurring maintenance tasks"
   homepage "https://github.com/halbotley/duely"
-  url "https://github.com/halbotley/duely.git", tag: "v1.0.0"
+  url "https://github.com/halbotley/duely/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   head "https://github.com/halbotley/duely.git", branch: "main"
 
-  depends_on xcode: ["14.0", :build]
   depends_on :macos
 
   def install
